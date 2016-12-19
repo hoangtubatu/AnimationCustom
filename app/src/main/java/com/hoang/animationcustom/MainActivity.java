6778493bb5fe62725911f4ee8f8dbce4e2dc8c26
Hoang-PC <hoangtubatu.nth@gmail.com>
2016-12-20 03:39:56 +0700

    - done listview motionEven

--- app/src/main/java/com/hoang/animationcustom/MainActivity.java
+++ app/src/main/java/com/hoang/animationcustom/MainActivity.java
@@ -1,11 +1,12 @@
 package com.hoang.animationcustom;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.AbsListView;
+import android.view.ViewTreeObserver;
+import android.view.animation.OvershootInterpolator;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -24,42 +25,29 @@ public class MainActivity extends AppCompatActivity implements  View.OnTouchList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, android.R.id.text1, _values);
 
         _listView = (ListView) findViewById(R.id.list);
-        _defaultListViewPosY = _listView.getTranslationY();
         _listView.setAdapter(adapter);
         _listView.setOnTouchListener(this);
-        _listView.setOnScrollListener(new AbsListView.OnScrollListener(){
-            boolean mIsScrollingUp;
-            int mLastFirstVisibleItem;
-
-            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-
-            }
-
-            public void onScrollStateChanged(AbsListView view, int scrollState) {
-                if(scrollState == SCROLL_STATE_IDLE)
-                    Log.i("a", "scrolling stopped...");
-
-                if (view.getId() == _listView.getId()) {
-                    final int currentFirstVisibleItem = _listView.getFirstVisiblePosition();
-                    if (currentFirstVisibleItem > mLastFirstVisibleItem) {
-                        mIsScrollingUp = false;
-                        Log.i("a", "scrolling down...");
-                    } else if (currentFirstVisibleItem < mLastFirstVisibleItem) {
-                        mIsScrollingUp = true;
-                        Log.i("a", "scrolling up...");
-                    }
-
-                    mLastFirstVisibleItem = currentFirstVisibleItem;
+        _listView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                _limitTopLv = (int)_listView.getTranslationY();
+                _limitBotLv = _listView.getHeight()- _listView.getHeight()/8;
+                if (Build.VERSION.SDK_INT < 16) {
+                    _listView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                } else {
+                    _listView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
                 }
             }
         });
     }
 
-    float _defaultListViewPosY;// the position of listview when create first time
+    int _limitTopLv;// the position of listview when create first time
+    int _limitBotLv; // the position of listview when move to bottom
     private int _yDelta;
     int _actionDownListPosY; // raw position Y when DOWN
     int _curMoveListPosY; // raw position Y when MOVE
@@ -67,14 +55,14 @@ public class MainActivity extends AppCompatActivity implements  View.OnTouchList
     boolean _scrollingList;// detect if list view is scrolling
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
-        if(!listIsAtTop()){
+         if(!listIsAtTop()){
             _scrollingList =true;
             return false;/*when list not at the top, remove onTouch, keep list scrollable*/
         }
 
         switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
             case MotionEvent.ACTION_DOWN:
-                if(_scrollingList ==true) _scrollingList =false;
+                if(true == _scrollingList) _scrollingList =false;
 
                 _actionDownListPosY= (int)motionEvent.getRawY();
                 _preMoveListPosY= (int)motionEvent.getRawY();
@@ -82,6 +70,11 @@ public class MainActivity extends AppCompatActivity implements  View.OnTouchList
                 _yDelta = _actionDownListPosY - (int)_listView.getTranslationY();
                 break;
             case MotionEvent.ACTION_UP:
+                if(_listView.getY() <= _listView.getHeight()/2){
+                    _listView.animate().setDuration(500).translationY(_limitTopLv).setInterpolator(new OvershootInterpolator());
+                }else{
+                    _listView.animate().setDuration(500).translationY(_limitBotLv).setInterpolator(new OvershootInterpolator());
+                }
                 break;
             case MotionEvent.ACTION_POINTER_DOWN:
                 break;
@@ -109,11 +102,12 @@ public class MainActivity extends AppCompatActivity implements  View.OnTouchList
         return _listView.getChildAt(0).getTop() == 0;
     }
     private boolean checkAndSetLimitMoveTop(){
-        if(_defaultListViewPosY>=_listView.getTranslationY()){
-            _listView.setTranslationY(_defaultListViewPosY);
+        if(_limitTopLv >=_listView.getTranslationY()){
+            _listView.setTranslationY(_limitTopLv);
             return true;
         }
 
         return false;
     }
+
 }
